1d2c929dac05610c4bf82bdf7de05a366a93806d
Varun Kulkarni <varunkulkarni@gmail.com>
2025-10-20 15:32:37 -0400

    get migration scripts running

--- migrations/1760969251819_setup-extensions.ts
+++ migrations/1760969251819_setup-extensions.ts
@@ -1,9 +1,9 @@
-import { MigrationBuilder } from 'node-pg-migrate';
+import { MigrationBuilder } from "node-pg-migrate";
 
 export const up = (pgm: MigrationBuilder) => {
-    pgm.createExtension("uuid-ossp")
+  pgm.createExtension("uuid-ossp");
 };
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
-    pgm.dropExtension("uuid-ossp")
+  pgm.dropExtension("uuid-ossp");
 }
--- migrations/1760987236807_initial-tables.ts
+++ migrations/1760987236807_initial-tables.ts
@@ -1,51 +1,55 @@
-import { MigrationBuilder, PgLiteral } from 'node-pg-migrate';
+import { MigrationBuilder, PgLiteral } from "node-pg-migrate";
 
 export const up = (pgm: MigrationBuilder) => {
-    pgm.createTable('users', {
-        user_id: {
-            type: 'uuid',
-            primaryKey: true,
-            default: new PgLiteral('uuid_generate_v4()'),
-            notNull: true,
-        },
-        name: { type: 'varchar(255)', notNull: true },
-        createdAt: {
-            type: 'timestamp',
-            notNull: true,
-            default: pgm.func('current_timestamp'),
-        },
-    });
-    pgm.createIndex('users', 'user_id');
-    pgm.createTable('channels', {
-        channel_id: {
-            type: 'uuid',
-            primaryKey: true,
-            default: new PgLiteral('uuid_generate_v4()'),
-            notNull: true,
-        },
-        name: { type: 'varchar(255)', notNull: true },
-        createdAt: {
-            type: 'timestamp',
-            notNull: true,
-            default: pgm.func('current_timestamp'),
-        },
-    })
-    pgm.createIndex('channels', 'channel_id');
-    pgm.createTable('channel_members', {
-        channel_id: {
-            type: 'uuid',
-            notNull: true,
-            references: '"channels"',
-            onDelete: 'CASCADE',
-        },
-        user_id: {
-            type: 'uuid',
-            notNull: true,
-            references: '"users"',
-            onDelete: 'CASCADE',
-        }
-    })
-    pgm.createIndex('channel_members', ['channel_id', 'user_id']);
+  pgm.createTable("users", {
+    user_id: {
+      type: "uuid",
+      primaryKey: true,
+      default: new PgLiteral("uuid_generate_v4()"),
+      notNull: true,
+    },
+    name: { type: "varchar(255)", notNull: true },
+    createdAt: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+  pgm.createIndex("users", "user_id");
+  pgm.createTable("channels", {
+    channel_id: {
+      type: "uuid",
+      primaryKey: true,
+      default: new PgLiteral("uuid_generate_v4()"),
+      notNull: true,
+    },
+    name: { type: "varchar(255)", notNull: true },
+    createdAt: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  });
+  pgm.createIndex("channels", "channel_id");
+  pgm.createTable("channel_members", {
+    channel_id: {
+      type: "uuid",
+      notNull: true,
+      references: '"channels"',
+      onDelete: "CASCADE",
+    },
+    user_id: {
+      type: "uuid",
+      notNull: true,
+      references: '"users"',
+      onDelete: "CASCADE",
+    },
+  });
+  pgm.createIndex("channel_members", ["channel_id", "user_id"]);
 };
 
-export async function down(pgm: MigrationBuilder): Promise<void> {}
+export async function down(pgm: MigrationBuilder): Promise<void> {
+  pgm.dropTable("users", { cascade: true });
+  pgm.dropTable("channels", { cascade: true });
+  pgm.dropTable("channel_members", { cascade: true });
+}
