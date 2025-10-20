f45e60807407cd764aea7aba4480a94628f97124
Varun Kulkarni <varunkulkarni@gmail.com>
2025-10-20 16:28:02 -0400

    add messages table

--- migrations/1760987236807_initial-tables.ts
+++ migrations/1760987236807_initial-tables.ts
@@ -1,6 +1,7 @@
 import { MigrationBuilder, PgLiteral } from "node-pg-migrate";
 
 export const up = (pgm: MigrationBuilder) => {
+  // Users
   pgm.createTable("users", {
     user_id: {
       type: "uuid",
@@ -16,6 +17,8 @@ export const up = (pgm: MigrationBuilder) => {
     },
   });
   pgm.createIndex("users", "user_id");
+
+  // Channels
   pgm.createTable("channels", {
     channel_id: {
       type: "uuid",
@@ -31,6 +34,8 @@ export const up = (pgm: MigrationBuilder) => {
     },
   });
   pgm.createIndex("channels", "channel_id");
+
+  // Channel members
   pgm.createTable("channel_members", {
     channel_id: {
       type: "uuid",
@@ -43,17 +48,50 @@ export const up = (pgm: MigrationBuilder) => {
       notNull: true,
       references: '"users"',
       onDelete: "CASCADE",
-    }
+    },
   });
   pgm.addConstraint("channel_members", "channel_members_pkey", {
     primaryKey: ["channel_id", "user_id"],
-  })
+  });
   pgm.createIndex("channel_members", ["channel_id", "user_id"]);
 
+  // Messages
+  pgm.createTable("messages", {
+    message_id: {
+      type: "uuid",
+      primaryKey: true,
+      default: new PgLiteral("uuid_generate_v4()"),
+      notNull: true,
+    },
+    user_id: {
+      type: "uuid",
+      notNull: true,
+      references: '"users"',
+      onDelete: "CASCADE",
+    },
+    channel_id: {
+      type: "uuid",
+      notNull: true,
+      references: '"channels"',
+      onDelete: "CASCADE",
+    },
+    message: {
+      type: "text",
+      notNull: true,
+    },
+    createdAt: {
+      type: "timestamp",
+      notNull: true,
+      default: pgm.func("current_timestamp"),
+    },
+  })
+  pgm.createIndex("messages", ["message_id"]);
+  pgm.createIndex("messages", ["channel_id", "createdAt"]);
 };
 
 export async function down(pgm: MigrationBuilder): Promise<void> {
   pgm.dropTable("users", { cascade: true });
   pgm.dropTable("channels", { cascade: true });
   pgm.dropTable("channel_members", { cascade: true });
+  pgm.dropTable("messages", { cascade: true });
 }
