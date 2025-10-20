43df201f5068b629f523c46cca9aa0d7559c539d
Varun Kulkarni <varunkulkarni@gmail.com>
2025-10-19 23:56:30 -0400

    install and set up prettier

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,6 +4,7 @@ import eslint from '@eslint/js';
 import { defineConfig } from 'eslint/config';
 import tseslint from 'typescript-eslint';
 import globals from "globals";
+import eslintConfigPrettier from "eslint-config-prettier";
 
 export default defineConfig(
     eslint.configs.recommended,
@@ -14,5 +15,6 @@ export default defineConfig(
                 ...globals.node
             }
         }
-    }
+    },
+    eslintConfigPrettier,
 );
