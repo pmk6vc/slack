0d66e06d931e7f18ca8edfa2038214317018be47
Varun Kulkarni <varunkulkarni@gmail.com>
2025-10-24 13:43:04 -0400

    format

--- src/db/LocalDevPostgresConfig.ts
+++ src/db/LocalDevPostgresConfig.ts
@@ -1,15 +1,17 @@
+import { PostgreSqlContainer } from "@testcontainers/postgresql";
 import PostgresConfig from "./PostgresConfig";
-import {PostgreSqlContainer} from "@testcontainers/postgresql";
 
 export default class LocalDevPostgresConfig extends PostgresConfig {
-    static override async getInstance(): Promise<LocalDevPostgresConfig> {
-        const container = await new PostgreSqlContainer("postgres:17-alpine").start();
-        return super.getInstance({
-            username: container.getUsername(),
-            password: container.getPassword(),
-            host: container.getHost(),
-            port: container.getPort(),
-            database: container.getDatabase(),
-        });
-    }
+  static override async getInstance(): Promise<LocalDevPostgresConfig> {
+    const container = await new PostgreSqlContainer(
+      "postgres:17-alpine",
+    ).start();
+    return super.getInstance({
+      username: container.getUsername(),
+      password: container.getPassword(),
+      host: container.getHost(),
+      port: container.getPort(),
+      database: container.getDatabase(),
+    });
+  }
 }
